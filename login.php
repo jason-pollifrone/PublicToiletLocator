7e073ef0df3c3817d3d208eefc365ba54282cd4f
Hung Ly <rmit-s3430923-hung-ly@users.noreply.github.com>
2017-09-12 15:55:32 +1000

    Add files via upload
    V10

---
+++ login.php
@@ -0,0 +1,38 @@
+<html>
+<body>
+
+<?php
+	
+	//Receive input from client side
+	$input = $_POST['username'] . $_POST['password'];
+
+	
+    
+
+	//check if the input exist
+	$exist = 0;
+
+//read the file line by line
+	foreach(file('admin.txt') as $line) {
+		//compare the content of the input and the line
+		if($line == $input."\n"){
+			$exist = 1;
+			break;
+		}
+	}
+	
+	if($exist == 1){
+        echo "Login Successful. <br><br>You will be redirected to Admin page shortly..";     
+        header( "Refresh:5; url=toiletwayfinder(admin).php", true, 303);
+	}else
+        {
+
+		echo "Account not found returning to home page.";
+        header( "Refresh:5; url=home.html", true, 303);
+	}
+    
+?>
+
+</body>
+</html>
+	
---
+++ register.php
@@ -0,0 +1,42 @@
+<html>
+<body>
+
+<?php
+	
+	//Receive input from client side
+	$input = $_POST['username'] . $_POST['password'];
+
+	
+    
+
+	//check if the input exist
+	$exist = 0;
+
+//read the file line by line
+	foreach(file('admin.txt') as $line) {
+        $line = explode(",", $line);
+		//compare the content of the input and the line
+		if($line == $input."\n"){
+			$exist = 1;
+			break;
+		}
+	}
+	
+	if($exist == 1){
+		echo "The username already exist. <br/><br/>Please enter another one via <a href='register.html'>Register</a>";
+	}else{
+		//open a file named "database.txt"
+		$file = fopen("admin.txt","a");
+		//insert this input (plus a newline) into the database.txt
+		fwrite($file,$input."\n");
+		//close the "$file"
+		fclose($file);
+		echo "Admin account is registered. <br><br>You will be redirected to login page shortly..";
+        header( "Refresh:5; url=home.html", true, 303);
+	}
+    
+?>
+
+</body>
+</html>
+	
